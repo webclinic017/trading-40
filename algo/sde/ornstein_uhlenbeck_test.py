91e3ce1c3f1502af6b5da9bae4f47092e11fdc61
JoeRRPhillips <joe.rr.phillips@gmail.com>
2022-11-10 19:11:15 +0000

    test: OU

---
+++ algo/sde/ornstein_uhlenbeck_test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from algo.sde.ornstein_uhlenbeck import OrnsteinUhlenbeck
+from algo.sde.ornstein_uhlenbeck_optimisation import OUParams
+
+
+if __name__ == "__main__":
+    ou_params = OUParams(
+        theta=0.1405849503082211,
+        mu=80.49491464625557,
+        sigma=0.12652352136635514,
+        sigma_sq=0.016008201458942526,
+        sums=(154.57621525792626, 154.59653199802102, 21.629070312663373, 21.634927968936843, 21.630477363692105)
+    )
+
+    test_path = "/Users/joe/code/trading/algo/sde/data/ou_test.csv"
+    x = pd.read_csv(test_path).to_numpy()[:, 0]
+
+    ou_model = OrnsteinUhlenbeck(X_0=x[0], theta=ou_params.theta, k=ou_params.mu, sigma=ou_params.sigma)
+    ou_process_simulated = ou_model(num_samples=len(x))
