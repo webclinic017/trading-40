4547264d669a6f7e9c0cb3b707fe1425d8b64ef4
JoeRRPhillips <joe.rr.phillips@gmail.com>
2022-11-16 22:34:00 +0000

    chore: update message logged

--- algo/cointegration/augmented_dickey_fuller.py
+++ algo/cointegration/augmented_dickey_fuller.py
@@ -6,7 +6,8 @@ def print_adf_results(adf: Tuple):
     Args:
         adf: statsmodels.tsa.stattools.adfuller result object.
     """
-    _msg = lambda l: f"REJECT the hypothesis that S1 and S2 are NOT cointegrated at the {l} significance level"
+    _msg = lambda l: f"REJECT the null hypothesis of a unit root in the residuals at the {l} significance level." \
+                     f"S1 and S2 are cointegrated."
 
     test_pass = False
     test_score = adf[0]
